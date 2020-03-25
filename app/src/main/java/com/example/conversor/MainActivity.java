141a7a4ed33473f4c68b8d3cd58323e9f00ebee7
abellechaves <59939104+abellechaves@users.noreply.github.com>
2020-03-25 20:51:18 -0300

    Alterações visuais e evento de botão.

--- app/src/main/java/com/example/conversor/MainActivity.java
+++ app/src/main/java/com/example/conversor/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.conversor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static com.example.conversor.R.*;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(layout.activity_main);
+
+        setTitle("Conversor");
+
+    }
+
+
+    public void cliquebotao(View view) {
+        Log.d("texto", "clicou");
+        EditText txt = findViewById(id.Valor);
+        Editable valor = txt.getText();
+        Log.d("texto", "valor: " + valor);
+        float valorDigitado = 0;
+        if (valor.length() > 0) {
+             valorDigitado = Float.parseFloat(String.valueOf(valor));
+        }
+        Intent intent = new Intent(this, Tela2.class);
+        intent.putExtra("resp", valorDigitado);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/conversor/Tela2.java
+++ app/src/main/java/com/example/conversor/Tela2.java
@@ -2,7 +2,12 @@ package com.example.conversor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Tela2 extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class Tela2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
+
+        setTitle("Temperatura Convertida");
+
+        Intent intent = getIntent();
+        float valorRecebido = intent.getFloatExtra("resp",0);
+
+        Log.d("texto","vaslor recebido: " + valorRecebido);
+
+        float valorConversao = ((valorRecebido*9)/5) + 32;
+        TextView txt = (TextView) findViewById(R.id.textView2);
+        txt.setText("Valor convertido = " + valorConversao);
     }
 }
